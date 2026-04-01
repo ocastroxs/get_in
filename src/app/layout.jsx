20f80bf2cbfcf5ae98a6f3a3831f86fff18f249f
ocastroxs <hgcastroaquino2009@gmail.com>
2026-04-01 14:43:20 -0300

    refactor: atualizar idioma para pt-br e remover conteúdo desnecessário da página inicial

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Pl, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -22,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="pt-br"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
