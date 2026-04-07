308f8221fc51426ed3def8908140ed48ba6a8339
ocastroxs <hgcastroaquino2009@gmail.com>
2026-04-07 14:20:14 -0300

    feat: adicionar componente de checkbox personalizado e atualizar formulário de login

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -8,6 +8,8 @@ import {
   FieldSeparator,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import CheckboxStandard6 from "./checkbox-standard-6";
+import { ShieldCheck } from "lucide-react";
 
 export function LoginForm({
   className,
@@ -16,8 +18,8 @@ export function LoginForm({
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <FieldGroup>
-        <div className="flex flex-col gap-1 --font-heading">
-          <h1 className="text-2xl font-bold">Bem-vindo de volta</h1>
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl font-bold --font-heading">Bem-vindo de volta</h1>
           <p className="text-sm text-balance text-muted-foreground">
             Acesse o painel com suas credenciais de segurança.
           </p>
@@ -35,14 +37,26 @@ export function LoginForm({
           <div className="flex items-center">
             <FieldLabel htmlFor="password">Password</FieldLabel>
             <a href="#" className="ml-auto text-sm underline-offset-4 hover:underline">
-              Forgot your password?
+              Esqueceu sua senha?
             </a>
           </div>
           <Input id="password" type="password" required className="bg-background" />
         </Field>
         <Field>
-          <Button type="submit">Login</Button>
+          <Button type="submit">Entrar no Sistema</Button>
+          <div className="flex">
+            <CheckboxStandard6 />
+            <a href="#" className="ml-auto text-sm underline-offset-4 hover:underline">
+              Esqueceu sua senha?
+            </a>
+          </div>
         </Field>
+        <div className="">
+          <div className="">
+            <ShieldCheck />
+          </div>
+          <p>Conexão Segura • Dados Criptografados • Acesso Auditado</p>
+        </div>
       </FieldGroup>
     </form>
   );
