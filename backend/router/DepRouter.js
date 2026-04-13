cb6f138f2322c173fc5f7ed9b1fbc5ea5a2dae27
Zer0o0 <henrique.cosmemk4@gmail.com>
2026-04-13 16:15:54 -0300

    finalizando rotas

--- backend/router/DepRouter.js
+++ backend/router/DepRouter.js
@@ -3,7 +3,13 @@ import DepartamentoController from '../controllers/DepartamentoController.js';
 const router = express.Router();
 
 router.get('/', DepartamentoController.read);
+router.get('/:id', DepartamentoController.readById);
 router.post('/', DepartamentoController.create);
+router.put('/:id', DepartamentoController.update);
+router.delete('/:id', DepartamentoController.delete);
+
+
+
 
 
 export default router;
--- backend/router/FuncRouter.js
+++ backend/router/FuncRouter.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 import FuncController from '../controllers/FuncController.js';
 
-router.get('/read', FuncController.Read);
-router.get('/read/:id', FuncController.ReadId);
-router.post('/create', FuncController.Create);
-router.put('/update/:id', FuncController.Update);
-router.delete('/delete/:id', FuncController.Delete);
+router.get('/', FuncController.Read);
+router.get('/:id', FuncController.ReadId);
+router.get('/name/:id', FuncController.ReadName);
+router.get('/cpf/:id', FuncController.ReadCpf);
+router.post('/', FuncController.Create);
+router.put('/:id', FuncController.Update);
+router.delete('/:id', FuncController.Delete);
+
 
 export default router;
